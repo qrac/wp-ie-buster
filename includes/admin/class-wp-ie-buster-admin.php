9da7810d28557822e3f37849b3f3964ef8d97e60
Ko Takagi <knockoutmarch@gmail.com>
2019-02-11 22:07:54 +0900

    Add i18n

--- includes/admin/class-wp-ie-buster-admin.php
+++ includes/admin/class-wp-ie-buster-admin.php
@@ -7,7 +7,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 class Wp_Ie_Buster_Admin {
 
   private $plugin_name;
-
   private $option_name;
 
   public function __construct( $plugin_name ) {
@@ -26,8 +25,8 @@ class Wp_Ie_Buster_Admin {
 
   public function admin_menu() {
     add_options_page(
-      __( 'WP IE Buster', $this->plugin_name ),
-      __( 'WP IE Buster', $this->plugin_name ),
+      __( 'WP IE Buster', 'wp-ie-buster' ),
+      __( 'WP IE Buster', 'wp-ie-buster' ),
       'manage_options',
       $this->plugin_name . '-settings',
       [ $this, 'display' ]
@@ -41,27 +40,27 @@ class Wp_Ie_Buster_Admin {
     );
     add_settings_section(
       'basic_settings',
-      __( '基本設定', $this->plugin_name ),
+      __( 'Basic Settings', 'wp-ie-buster' ),
       null,
       $this->plugin_name
     );
     add_settings_field(
       'main_text',
-      __( 'メインテキスト', $this->plugin_name ),
+      __( 'Main text', 'wp-ie-buster' ),
       [ $this, 'main_text_callback' ],
       $this->plugin_name,
       'basic_settings'
     );
     add_settings_field(
       'link_text',
-      __( 'リンクテキスト', $this->plugin_name ),
+      __( 'Link text', 'wp-ie-buster' ),
       [ $this, 'link_text_callback' ],
       $this->plugin_name,
       'basic_settings'
     );
     add_settings_field(
       'url',
-      __( 'URL', $this->plugin_name ),
+      __( 'URL', 'wp-ie-buster' ),
       [ $this, 'url_callback' ],
       $this->plugin_name,
       'basic_settings'
@@ -75,8 +74,8 @@ class Wp_Ie_Buster_Admin {
       name="<?php echo $this->option_name; ?>[main_text]"
       type="text"
       id="main_text"
-      placeholder="<?php esc_attr_e( Wp_Ie_Buster::get_default_main_text() ); ?>"
-      value="<?php esc_attr_e( $main_text ); ?>"
+      placeholder="<?php echo esc_attr( Wp_Ie_Buster::get_default_main_text() ); ?>"
+      value="<?php echo esc_attr( $main_text ); ?>"
       class="regular-text"
     >
     <?php
@@ -89,8 +88,8 @@ class Wp_Ie_Buster_Admin {
       name="<?php echo $this->option_name; ?>[link_text]"
       type="text"
       id="link_text"
-      placeholder="<?php esc_attr_e( Wp_Ie_Buster::get_default_link_text() ); ?>"
-      value="<?php esc_attr_e( $link_text ); ?>"
+      placeholder="<?php echo esc_attr( Wp_Ie_Buster::get_default_link_text() ); ?>"
+      value="<?php echo esc_attr( $link_text ); ?>"
       class="regular-text"
     >
     <?php
@@ -103,8 +102,8 @@ class Wp_Ie_Buster_Admin {
       name="<?php echo $this->option_name; ?>[url]"
       type="text"
       id="url"
-      placeholder="<?php esc_attr_e( Wp_Ie_Buster::get_default_url() ); ?>"
-      value="<?php esc_attr_e( $url ); ?>"
+      placeholder="<?php echo esc_attr( Wp_Ie_Buster::get_default_url() ); ?>"
+      value="<?php echo esc_attr( $url ); ?>"
       class="regular-text"
     >
     <?php
@@ -113,7 +112,7 @@ class Wp_Ie_Buster_Admin {
   function display() {
     ?>
     <form action='options.php' method='post'>
-      <h1><?php _e( 'WP IE Buster', $this->plugin_name ); ?></h1>
+      <h1><?php _e( 'WP IE Buster', 'wp-ie-buster' ); ?></h1>
       <?php
       settings_fields( $this->plugin_name );
       do_settings_sections( $this->plugin_name );
