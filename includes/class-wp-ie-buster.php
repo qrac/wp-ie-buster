abd4709b8729fa406c2d253020adb7dadb54d3fc
Qrac <qrac.jp@gmail.com>
2019-02-11 12:08:46 +0900

    Fix

--- includes/class-wp-ie-buster.php
+++ includes/class-wp-ie-buster.php
@@ -34,7 +34,6 @@ class Wp_Ie_Buster {
       $link_text = ( ! empty( $options['link_text'] ) ) ? $options['link_text'] : self::$default_link_text;
       $url       = ( ! empty( $options['url'] ) ) ? $options['url'] : self::$default_url;
       echo '
-    }
     <div id="ie-buster-app" style="position: fixed; top: 0px; left: 0; width: 100%; padding: 16px; box-sizing: border-box; z-index: 999999;">
       <div style="position: relative; width: 100%; max-width:866px; margin: 0 auto; padding: 16px 20px; background-color: #fff; box-shadow: rgba(0, 0, 0, 0.4) 0px 0px 5px 0px; box-sizing: border-box; font-family: SegoeUI, Meiryo, sans-serif;">
         <p style="display: block; float: left; width: 100%; max-width: 664px; margin: 0; color: #000; font-size: 14px; font-weight: 400; line-height: 1.5;">' . esc_html( $main_text ) . '</p>
