cc6ba9adcd213c19a782086e7c9ab71303e0335d
Qrac <qrac.jp@gmail.com>
2021-02-13 17:18:02 +0900

    Allow Microsoft Edge protocol

--- includes/class-wp-ie-buster.php
+++ includes/class-wp-ie-buster.php
@@ -43,11 +43,13 @@ class Wp_Ie_Buster {
       $main_text = ( ! empty( $options['main_text'] ) ) ? $options['main_text'] : self::$default_main_text;
       $link_text = ( ! empty( $options['link_text'] ) ) ? $options['link_text'] : self::$default_link_text;
       $url       = ( ! empty( $options['url'] ) ) ? $options['url'] : self::$default_url;
+      $protocols = array( 'http', 'https', 'microsoft-edge' );
+      $target    = preg_match( '/^microsoft-edge/', $url) ? '_self' : '_blank';
       echo '
     <div id="ie-buster-app" style="position: fixed; top: 0px; left: 0; width: 100%; padding: 16px; box-sizing: border-box; z-index: 999999;">
       <div style="position: relative; width: 100%; max-width:866px; margin: 0 auto; padding: 16px 20px; background-color: #fff; box-shadow: rgba(0, 0, 0, 0.4) 0px 0px 5px 0px; box-sizing: border-box; font-family: SegoeUI, Meiryo, sans-serif;">
         <p style="display: block; float: left; width: 100%; max-width: 664px; margin: 0; color: #000; font-size: 14px; font-weight: 400; line-height: 1.5;">' . esc_html( $main_text ) . '</p>
-        <a style="display: block; float: right; height: 36px; width: 154px; padding: 0 16px; background-color: rgb(0, 120, 212); box-sizing: border-box; color: #fff; font-size: 12px; font-weight: 400; line-height: 36px; text-align: center; text-decoration: none; white-space: nowrap;" href="' . esc_url( $url ) . '" target="_blank" rel="noopener noreferrer">' . esc_html( $link_text ) . '</a>
+        <a style="display: block; float: right; height: 36px; width: 154px; padding: 0 16px; background-color: rgb(0, 120, 212); box-sizing: border-box; color: #fff; font-size: 12px; font-weight: 400; line-height: 36px; text-align: center; text-decoration: none; white-space: nowrap;" href="' . esc_url( $url, $protocols ) . '" target="'. $target .'" rel="noopener noreferrer">' . esc_html( $link_text ) . '</a>
         <div style="clear: both;"></div>
       </div>
       <style>@supports not (-ms-high-contrast: none) {#ie-buster-app { display: none; }}</style>
